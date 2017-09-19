21cc226ce4785d9e1900c344c677db876ffe75e9
loopq <1325785802@qq.com>
2017-09-19 16:06:52 +0800

    upload

--- top/models.py
+++ top/models.py
@@ -3,25 +3,29 @@ from django.db.models.aggregates import Count
 from django.db import models
 from django.utils import timezone
 
+
 # Create your models here.
 
 class KeywordLogManager(models.Manager):
     def top_hourly(self):
         now = timezone.now()
         time_begin = now - datetime.timedelta(hours=1)
-        cursor = self.filter(log_time__gte=time_begin).values('keyword').annotate(pv=Count('ip', distinct=True)).order_by('-pv')
+        cursor = self.filter(log_time__gte=time_begin).values('keyword').annotate(
+            pv=Count('ip', distinct=True)).order_by('-pv')
         return cursor
 
     def top_daily(self):
         now = timezone.now()
         time_begin = now - datetime.timedelta(days=1)
-        cursor = self.filter(log_time__gte=time_begin).values('keyword').annotate(pv=Count('ip', distinct=True)).order_by('-pv')
+        cursor = self.filter(log_time__gte=time_begin).values('keyword').annotate(
+            pv=Count('ip', distinct=True)).order_by('-pv')
         return cursor
 
     def latest(self):
         now = timezone.now()
         time_begin = now - datetime.timedelta(hours=1)
         return self.filter(log_time__gte=time_begin).order_by('-log_time')
+
 
 class KeywordLog(models.Model):
     objects = KeywordLogManager()
@@ -34,13 +38,15 @@ class HashLogManager(models.Manager):
     def top_hourly(self):
         now = timezone.now()
         time_begin = now - datetime.timedelta(hours=1)
-        cursor = self.filter(log_time__gte=time_begin).values('hash_id').annotate(pv=Count('ip', distinct=True)).order_by('-pv')
+        cursor = self.filter(log_time__gte=time_begin).values('hash_id').annotate(
+            pv=Count('ip', distinct=True)).order_by('-pv')
         return cursor
 
     def top_daily(self):
         now = timezone.now()
         time_begin = now - datetime.timedelta(days=1)
-        cursor = self.filter(log_time__gte=time_begin).values('hash_id').annotate(pv=Count('ip', distinct=True)).order_by('-pv')
+        cursor = self.filter(log_time__gte=time_begin).values('hash_id', 'hash_name').annotate(
+            pv=Count('ip', distinct=True)).order_by('-pv')
         return cursor
 
 
@@ -49,4 +55,3 @@ class HashLog(models.Model):
     log_time = models.DateTimeField(auto_now_add=True, db_index=True)
     hash_id = models.PositiveIntegerField()
     ip = models.CharField(max_length=30)
-
