07212e417a473fc95cd3a17c876a8658fbc5e42d
loopq <1325785802@qq.com>
2017-11-24 16:51:47 +0800

    '修改时区和语言为中国相关'

--- top/admin.py
+++ top/admin.py
@@ -2,6 +2,12 @@ from django.contrib import admin
 
 from .models import *
 
+
 # Register your models here.
 
-admin.site.register([HashLog, KeywordLog])
+class YourAdmin(admin.ModelAdmin):
+    readonly_fields = ('log_time',)
+
+
+admin.site.register(KeywordLog, YourAdmin)
+admin.site.register(HashLog)
